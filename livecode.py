446bc5e28c76a7b54d3818c7ea6266b8b3355e76
Edward-Sakash <edward.sakash@yahoo.com>
2023-06-07 13:00:18 +0200

    q

--- livecode.py
+++ livecode.py
@@ -1,4 +1,4 @@
-l1 = [51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100]
+"""l1 = [51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100]
 print(len(l1))
 
 #Step 1
@@ -46,4 +46,60 @@ plt.xlabel('Input Size')
 plt.ylabel('Number of steps')
 plt.legend()
 plt.title('Binary Search')
-plt.show()
+plt.show()"""
+
+# Algorithmic thinking
+# Divide and Conquer
+
+
+print('mod :', 64 % 40)
+
+print('mod :', 104 % 40)
+
+def hcf(a, b):
+    remainder = a % b
+    if remainder == 0:
+        return b
+    else:
+        return str(remainder) + ' ' + str(hcf(b, remainder))
+
+print(hcf(64, 40))
+
+total_area = 64 * 40
+print(total_area)
+number_of_squares = total_area/(8**2)
+
+print(number_of_squares)
+
+###################################################
+
+
+
+#### Quicksort #####
+
+lst = [33, 15, 10] # we can pick for example 33; 33 will be my pivot element
+
+
+sorted = [15, 10] + [33] + []
+
+def quicksort(lst):
+    if len(lst) < 2:
+        return lst
+    else:
+        pivot = lst[1]
+        less = [num for num in lst[:1] + lst[2:] if num <= pivot]
+        greater = [num for num in lst[:1] + lst[2:] if num > pivot]
+        print('##############')
+        print(f"pivot: {pivot}")
+        print(f"less: {less}")
+        print(f"greater: {greater}")
+
+    return quicksort(less) + [pivot] + quicksort(greater) 
+
+#print(quicksort([10, 5, 2, 3, 4, 4, 20]))
+
+lst = [3, 7, 1, 9, 2, 6, 8, 5]
+print(lst)
+print(quicksort(lst))
+
+
